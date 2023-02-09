11e3655663f1c5d1f13f7f87e452a775f405c377
John Robertson <jwilliamrob@gmail.com>
2023-02-09 17:50:45 -0500

    checkpoint

--- config/plugins.js
+++ config/plugins.js
@@ -21,18 +21,6 @@ module.exports = ({ env }) => ({
     // ...
 });
 
-// module.exports = ({ env }) => ({
-//     // ...
-//     'users-permissions': {
-//       config: {
-//         jwt: {
-//           expiresIn: '7d',
-//         },
-//       },
-//     },
-//     // ...
-//   });
-
 module.exports = ({ env }) => ({
   // ...
   upload: {
@@ -42,6 +30,13 @@ module.exports = ({ env }) => ({
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
+      },
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
       },
       actionOptions: {
         upload: {},
--- src/api/event/controllers/custom.js
+++ src/api/event/controllers/custom.js
@@ -1,34 +1,39 @@
-const jwt_decode = require('jwt-decode');
+const jwt_decode = require("jwt-decode");
 
 module.exports = {
   // default res status is 404!!!!
 
   // FETCH ALL EVENTS AND THEIR CORRESPONDING TEE TIMES/PARTICIPANTS
-  async findAll(ctx) { // called by GET /hello 
+  async findAll(ctx) {
+    // called by GET /hello
     ctx.response.status = 200; // we could also send a JSON
-    const all = await strapi.entityService.findMany('api::event.event', {
+    const all = await strapi.entityService.findMany("api::event.event", {
       // populate: ['participants', 'tee_time.time_slots.tee-time', '  tee_time.time_slots.participants'],
-      populate: ['transactions', 'users', 'tee-time'],
-      fields: ['*']
+      populate: ["transactions", "users", "tee-time"],
+      fields: ["*"],
     });
 
     // Get users, teetimes, etc.
 
     ctx.response.body = {
-      data: all
+      data: all,
     };
   },
 
   async findUserEvents(ctx) {
     const { identifier } = ctx.request.query;
-    console.log({ id: identifier, ctx })
+    console.log({ id: identifier, ctx });
 
-    const event = await strapi.entityService.findOne('api::event.event', parseInt(identifier), {
-      populate: ['users'],
-      fields: ['*'],
-    });
+    const event = await strapi.entityService.findOne(
+      "api::event.event",
+      parseInt(identifier),
+      {
+        populate: ["users"],
+        fields: ["*"],
+      }
+    );
 
-    console.log(`[Event API] findUserEvents`, event)
+    console.log(`[Event API] findUserEvents`, event);
 
     ctx.response.body = event;
     ctx.response.status = 200;
@@ -36,55 +41,66 @@ module.exports = {
 
   // CREATE NEW EVENT AND TEE TIME
   async createNew(ctx) {
-    const { eventName, eventDescription } = ctx.request.body.data;
-    const apiEvent = 'api::event.event';
+    const { name, description, fee, date, max_users } = ctx.request.body.data;
+    const apiEvent = "api::event.event";
+
+    console.log("EVENT NEW START PAYLOAD", { name, description });
 
     let event = {
       id: null,
-      error: null
+      error: null,
     };
     let teeTime = {
       id: null,
-      error: null
-    };;
+      error: null,
+    };
 
-    const slug = eventName.toLowerCase().replaceAll(/[^a-z0-9]+/gi, "-");
-    console.log("[Event API] slug", slug)
+    const slug = name.toLowerCase().replaceAll(/[^a-z0-9]+/gi, "-");
+    console.log("[Event API] slug", slug);
 
-    await strapi.entityService.create(apiEvent, {
-      data: {
-        name: eventName,
-        publishedAt: new Date(),
-        slug: slug,
-        description: eventDescription,
-      },
-      fields: ['id', 'name', 'slug'] // Fields to be returned
-    }).then(res => {
-      console.log("[Event API] event res", res)
-      event.id = res.id
-      ctx.response.body = {
-        data: res
-      }
-      ctx.response.status = 200
-    }).catch(err => {
-      console.log("[Event API] event err", err)
-      event.error = err;
-      ctx.response.body = err
-      ctx.response.status = 500
-    });
+    await strapi.entityService
+      .create(apiEvent, {
+        data: {
+          publishedAt: new Date(),
+          slug,
+          name,
+          description,
+          fee,
+          date,
+          max_users,
+        },
+        fields: ["id", "name", "slug"], // Fields to be returned
+      })
+      .then((res) => {
+        console.log("[Event API] event res", res);
+        event.id = res.id;
+        ctx.response.body = {
+          data: res,
+        };
+        ctx.response.status = 200;
+      })
+      .catch((err) => {
+        console.log("[Event API] event err", err);
+        event.error = err;
+        ctx.response.body = err;
+        ctx.response.status = 500;
+      });
 
     if (event) {
-      await strapi.entityService.create('api::tee-time.tee-time', {
-        data: {
-          Name: eventName,
-          event: [event.id],
-          publishedAt: new Date(),
-        },
-      }).then(res => {
-        teeTime.id = res.id
-      }).catch(err => {
-        teeTime.error = err;
-      })
+      await strapi.entityService
+        .create("api::tee-time.tee-time", {
+          data: {
+            Name: name,
+            event: [event.id],
+            publishedAt: new Date(),
+          },
+        })
+        .then((res) => {
+          teeTime.id = res.id;
+        })
+        .catch((err) => {
+          teeTime.error = err;
+        });
     }
 
     // if (!event.id) {
@@ -123,24 +139,24 @@ module.exports = {
   // Rgister for events (create transaction, tee time, and )
   async register(ctx) {
     const token = jwt_decode(ctx.request.header.authorization);
-    const payloadEvents = ctx.request.body.events // Seed Data
-    const payloadType = ctx.request.body.type
+    const payloadEvents = ctx.request.body.events; // Seed Data
+    const payloadType = ctx.request.body.type;
 
     if (!payloadType && !payloadEvents) {
       ctx.response.status = 400;
-      ctx.response.message = 'Missing events and type.';
+      ctx.response.message = "Missing events and type.";
       return;
     }
 
     if (!payloadEvents) {
       ctx.response.status = 400;
-      ctx.response.message = 'Missing events.';
+      ctx.response.message = "Missing events.";
       return;
     }
 
     if (!payloadType) {
       ctx.response.status = 400;
-      ctx.response.message = 'Missing type.';
+      ctx.response.message = "Missing type.";
       return;
     }
 
@@ -158,55 +174,57 @@ module.exports = {
       total: 0, // get from events
       success: true, // once the payment returns successfully set this
       user: [token.id],
-      type: payloadType
+      type: payloadType,
     };
 
-    // Get and update all events 
-    const events = await strapi.entityService.findMany('api::event.event', {
+    // Get and update all events
+    const events = await strapi.entityService.findMany("api::event.event", {
       // populate: ['participants', 'tee_time.time_slots.tee-time', '  tee_time.time_slots.participants'],
       filters: {
         id: {
-          $in: payloadEvents
-        }
+          $in: payloadEvents,
+        },
       },
-      populate: ['transactions', 'users'],
-      fields: ['id', 'Fee']
-    })
+      populate: ["transactions", "users"],
+      fields: ["id", "Fee"],
+    });
 
-    console.log('Events Before', events)
+    console.log("Events Before", events);
 
     // Create transaction total.
     transactionData.total = events.reduce((accumulator, object) => {
       return accumulator + object.fee;
-    }, 0)
+    }, 0);
 
     // Create new transaction
-    const transactionNew = await strapi.entityService.create('api::transaction.transaction', {
-      data: transactionData,
-    })
+    const transactionNew = await strapi.entityService.create(
+      "api::transaction.transaction",
+      {
+        data: transactionData,
+      }
+    );
 
     // - Add user and transaction to existing arrays
     events.forEach((e) => {
-      strapi.entityService.update('api::event.event', e.id, {
+      strapi.entityService.update("api::event.event", e.id, {
         data: {
-          users: [...e.users, token.id]
+          users: [...e.users, token.id],
         },
-      })
-    })
+      });
+    });
 
     // Update new transaction after payment resolves.
-    console.log('Events After', events)
+    console.log("Events After", events);
     // ctx.response.body.events = events
     // ctx.response.body.data = transactionNew
     ctx.response.body = {
       events,
-      transactionNew
-    }
+      transactionNew,
+    };
     ctx.response.status = 200;
 
     if (transactionNew && events) {
-      console.log('Transaction created.')
+      console.log("Transaction created.");
     }
-  }
+  },
 };
-
